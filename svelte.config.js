9c56c6a4636b056630f1712d69a9f51c7d04a701
Tolga Yonca <ynctlg@gmail.com>
2022-02-03 13:42:30 +0300

    ..

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import vercel from '@sveltejs/adapter-vercel';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +10,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: vercel(external),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
